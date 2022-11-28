54627c44bc89c8f103d33284daa91370e37c9926
Muhammad Faheem Bunally <bunallmf@myumanitoba.ca>
2022-11-28 00:43:20 -0600

    added numeric input validation

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// var fs = require("fs");
-
 function newBid() {
     // get value from text box
     var value = document.getElementById("bidAmount_textbox").value;
+
+    // check if value is numeric
+    if(!isNaN(value)) {
+        value = parseFloat(value).toFixed(2);
     
     // assign value to current bid
     document.getElementById("CurrentBid").innerHTML = "$" + value;
 
     // compare to highest bid
     var topBid = document.getElementById("topBid").innerHTML;
-    topBid = parseFloat(topBid.replace("$", ""));
-    
-    value = parseFloat(value);
+    topBid = parseFloat(topBid.replace("$", "")).toFixed(2);
 
     if(value > topBid){
         topBid = value;
@@ -19,6 +19,18 @@ function newBid() {
 
     document.getElementById("topBid").innerHTML = "$" + topBid;
 
+    // highest bid to price
+    var price = document.getElementById("Price").innerHTML;
+    price = parseFloat(price.replace("$", "")).toFixed(2);
+
+    if(topBid >= price) {
+        price = (120 * topBid / 100).toFixed(2);
+    }
+
+    document.getElementById("Price").innerHTML = "$" + price;
+
+    }
+    
     // clear input box
     document.getElementById("bidAmount_textbox").value = "";
 }
