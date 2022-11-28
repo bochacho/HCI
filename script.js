23cfdd84478c5936cd435ba5961bb57b6f12d4cd
Muhammad Faheem Bunally <bunallmf@myumanitoba.ca>
2022-11-27 23:47:26 -0600

    figuring out jump from browse to proper product page.

--- script.js
+++ script.js
@@ -23,10 +23,24 @@ function newBid() {
     document.getElementById("bidAmount_textbox").value = "";
 }
 
-function getProductPage() {
-    // console.log("next time debug");
-    // var page = fs.readFile("product_details.html");
-    // document.write(page);
+function getProductPage(product) {
+    console.log("next time debug");
+    // get image src
+    var image = document.getElementById("productImage").src;
+    //get price
+    var price = document.getElementById("Price").innerHTML;
+    price = parseFloat(price.replace("$", ""));
+    // get top bid
+    var topBid = document.getElementById("topBid").innerHTML;
+    topBid = parseFloat(topBid.replace("$", ""));
 
-    window.location.href = "product_details.html";
+    window.location.assign("product_details.html");
+
+    console.log("hello world");
+
+    switch(product){
+        case 'AirJordan1':
+            console.log(image);
+            document.getElementById("productImage").src = image;
+    }
 };
